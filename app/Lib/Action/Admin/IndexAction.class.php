e8113676ecac84723a83e5228fd1bc305b79b127
daidong <daidong@zmeng123.com>
2015-02-26 11:28:55 +0800

    课程管理OK

--- app/Lib/Action/Admin/IndexAction.class.php
+++ app/Lib/Action/Admin/IndexAction.class.php
@@ -13,30 +13,94 @@ class IndexAction extends BaseAction
     }
 
     public function index(){
-        if(IS_POST){
-            $data['ip'] = get_client_ip();
-            
+    	$ip = get_client_ip();
+    	$ipwhite = array('127.0.0.1','localhost','::1','0.0.0.0');
+        if(!in_array($ip,$ipwhite)){
+            exit('非法');
         }else{
-            
+            $this->display();
         }
         
     }
+    /*****************课程**************************/
+    //课程设置
+    public function course(){
+    	$course = M('course')->select();
+    	$this->assign('course',$course);
+    	$this->display();
+    }
+    //新增
+    public function courseAdd(){
+    	if(IS_POST){
+    		$data['name'] = $this->_post('name');
+    		$data['starttime'] = $this->_post('starttime');
+    		$data['scorerule'] = $this->_post('scorerule');
+    		$data['teacher'] = $this->_post('teacher');
+    		$id = M('course')->add($data);
+    		if($id){
+    			//echo json_encode(array('status'=>1,'msg'=>'Success!'));
+    			echo "添加成功";
+    		}else{
+    			//echo json_encode(array('status'=>0,'msg'=>'Failed!'));
+    			//echo "Failed";
+    			echo "添加失败";
 
+    		}
+    		return;
+    	}else{
+    		$this->display();
+    	}
+    }
+
+    //课程修改
+    public function courseEdit(){
+    	$cid = $this->_get('cid');
+    	if(IS_POST){
+    		$data['name'] = $this->_post('name');
+    		$data['starttime'] = $this->_post('starttime');
+    		$data['scorerule'] = $this->_post('scorerule');
+    		$data['teacher'] = $this->_post('teacher');
+    		$rid = M('course')->where(array('id'=>$cid))->save($data);
+    		if($rid){
+    			//echo json_encode(array('status'=>1,'msg'=>'Success!'));
+    			echo "修改成功";
+    		}else{
+    			//echo json_encode(array('status'=>0,'msg'=>'Failed!'));
+    			//echo "Failed";
+    			echo "修改失败";
+    		}
+    		return;
+    	}else{
+    		$course = M('course')->where(array('id'=>$cid))->find();
+    		$this->assign('course',$course);
+    		$this->display();
+    	}
+    }
+
+    //课程修改
+    public function courseDel($id){
+    	if($id){
+    		$rid = M('course')->where(array('id'=>$id))->delete();
+    		if($rid){
+    			echo json_encode(array('status'=>0,'msg'=>'Success!'));
+    		}else{
+    			echo json_encode(array('status'=>-1,'msg'=>'Failed!'));
+    		}
+    	}else{
+    		echo json_encode(array('status'=>-2,'msg'=>'Failed!'));	
+    	}
+    	return;
+    }
+
+    
     private function checkLogin(){
-    	if(!session('adminLogin')!='10086'){
-    		$this->display('login');
+    	if(session('adminLogin')!='10086'){
+    		$this->redirect('Admin/Login/index');
     	}else{
     		return true;
     	}
     }
-
-    public function Login(){
-    	if(IS_POST){
-
-    	}else{
-    		$this->display('login');
-    	}
-    }
+    
     //根据class递归得到其分组
     private function getClassGroup($class){
         $parentGroup = M('Group')->where(array('classid'=>$class['id'],'parentid'=>'0'))->select();
---
+++ app/Lib/Action/Admin/LoginAction.class.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * 
+ */
+
+class LoginAction extends BaseAction
+{
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function Index(){
+
+    	if(IS_POST){
+    		//var_dump($_POST);
+    		$username = $this->_post('username');
+    		$password = $this->_post('password');
+    		$admin = M('admin')->where(array('username'=>$username,'password'=>md5($password)))->find();
+    		if(!empty($admin)){
+    			session('adminLogin',10086);
+    			M('admin')->where(array('username'=>$username,'password'=>md5($password)))->save(array('lastlogin'=>date('Y-m-d H:i:s')));
+    			$this->redirect('Admin/Index/index','',3, '页面跳转中...');
+    			return true;
+    		}
+    	}
+    	$this->display('login');
+    }
+}
+
